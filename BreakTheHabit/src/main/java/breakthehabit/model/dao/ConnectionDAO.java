47f701d7271939e4c402b841f3d47eeb36f7906d
Pedro Henrique Arruda <pedrohenriquearruda158@gmail.com>
2024-04-29 14:28:45 -0300

    Commit 29-04

--- BreakTheHabit/src/main/java/breakthehabit/model/dao/ConnectionDAO.java
+++ BreakTheHabit/src/main/java/breakthehabit/model/dao/ConnectionDAO.java
@@ -9,13 +9,9 @@ public class ConnectionDAO {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/";
 
-    public static Connection getConnection()
-            throws ClassNotFoundException, SQLException
-    {
-        String nomeBanco = "breakthehabit";
-        String usuario = "root";
-        String senha = "";
+    public static Connection getConnection(String nomeBanco, String usuario, String senha)
+            throws ClassNotFoundException, SQLException {
         Class.forName(DRIVER);
-        return DriverManager.getConnection(URL+nomeBanco, usuario, senha);
+        return DriverManager.getConnection(URL + nomeBanco, usuario, senha);
     }
 }
--- BreakTheHabit/src/main/java/breakthehabit/model/dao/RegistroDAO.java
+++ BreakTheHabit/src/main/java/breakthehabit/model/dao/RegistroDAO.java
@@ -1,4 +1,28 @@
 package breakthehabit.model.dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class RegistroDAO {
+
+    public boolean verificarEmail() {
+        try{
+            Connection conn = ConnectionDAO.getConnection("breakthehabit", "root", "");
+
+        } catch (ClassNotFoundException | SQLException e){
+            System.out.println("Não Conectou!");
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean insert(Connection conn, String nome, String email)
+            throws SQLException {
+
+        String sql = String.format("insert into usuario(nome, email)" + "values('%s', '%s');", nome, email);
+        Statement cmd = conn.createStatement();
+        cmd.executeUpdate(sql);
+        return true;
+    }
 }
